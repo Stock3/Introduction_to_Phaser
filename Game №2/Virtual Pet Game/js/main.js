3ba3db3fa98ee2fe8e0031c7d8d69851419ef246
Alex Mitskan <olexiy.mitskan@gmail.com>
2019-06-29 15:09:00 +0300

    Stat of Pet

--- Game №2/Virtual Pet Game/js/main.js
+++ Game №2/Virtual Pet Game/js/main.js
@@ -66,16 +66,29 @@ var GameState = {
 
         //nothing is selected
         this.selectedItem = null;
+
+        //the user interface (UI) is not blocked at the start
         this.uiBlocked = false;
+
+        var style = { font: '20px Arial', fill: '#fff'};
+        this.game.add.text(10, 20, 'Health:', style);
+        this.game.add.text(140, 20, 'Fun:', style);
+
+        this.healthText = this.game.add.text(80, 20, '', style);
+        this.funText = this.game.add.text(185, 20, '', style);
+
+        this.refreshStats();
     },
 
     pickItem: function(sprite, event){
 
+        //if the UI is blocked we can't pick an item
         if(!this.uiBlocked){
             console.log('pick item');
 
             this.clearSelection();
 
+            //alpha to indicate selection
             sprite.alpha = 0.4;
 
             this.selectedItem = sprite;
@@ -96,17 +109,24 @@ var GameState = {
 
             var petRotation = this.game.add.tween(this.pet);
 
+            //make the pet do two loops
             petRotation.to({angle: '+720'}, 1000);
 
             petRotation.onComplete.add(function(){
+                //release the UI
                 this.uiBlocked = false;
 
                 sprite.alpha = 1;
 
+                //increse the fun of the pet
                 this.pet.customParams.fun += 10;
-                console.log(this.pet.customParams.fun);
+
+                //update the visuals for the stats
+                this.refreshStats();
+
             }, this);
 
+            //start the tween animation
             petRotation.start();
         }
     },
@@ -157,11 +177,19 @@ var GameState = {
                     }
                 }
 
+                //update the visuals for the stats
+                this.refreshStats();
+
             }, this);
 
             //start the tween animation
             petMovement.start();
         }  
+    },
+
+    refreshStats: function(){
+        this.healthText.text = this.pet.customParams.health;
+        this.funText.text = this.pet.customParams.fun;
     }
 };
 
